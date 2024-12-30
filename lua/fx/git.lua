7d7473ba06451e61192f623c83c1b259372d081b
skepticfx <ahamed.nafeez@gmail.com>
2024-12-30 09:47:36 -0800

    add hunk based git signs

--- lua/fx/git.lua
+++ lua/fx/git.lua
@@ -1,2 +1,21 @@
+require('gitsigns').setup{
+    on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Actions
+        map('n', '<leader>hp', gitsigns.preview_hunk)
+        map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end)
+        map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+        map('n', '<leader>hd', gitsigns.diffthis)
+
+    end
+}
+
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 
